75d30a53a5edf38ef4f7527aa732be8041597008
DANTE VERDI GUTIERREZ <47573312@ort>
2023-05-08 10:09:22 -0300

    avanzado selectpaquete

--- Models/OrtWorld.cs
+++ Models/OrtWorld.cs
@@ -1,6 +1,6 @@
 class OrtWorld {
 
-static public int NumDestinos {get; private set;} = 10;
+
     static public List<string> ListaDestinos {get; private set;} = new List<string> {"Ushuaia", "Lima", "Ciudad del Cabo", "Berlin", "San Petersburgo", "Kabul", "San Fransisco", "Mar del Plata", "Avellaneda", "Isla Rapa Nui"};
     static public List<string> ListaHotles {get; private set;} = new List<string> {"hotel00.jfif","hotel01.jfif","hotel02.jfif","hotel03.jfif","hotel04.jfif","hotel06.jfif","hotel06.jfif","hotel05.jfif","hotel08.jfif","hotel09.jfif"};
     static public List<string> ListaAereos {get; private set;} =new List<string> {"aereos00.jfif", "aereos01.jfif", "aereos02.jfif", "aereos03.jpg", "aereos04.jfif", "aereos05.jfif", "aereos06.jfif", "aereos07.jfif", "aereos08.jfif", "aereos09.jfif"};
