7aa1833840fdfacb0f929b40b6e5fe9c862bdb24
DANTE VERDI GUTIERREZ <47573312@ort>
2023-05-08 08:57:59 -0300

    controller index y selectpaquete

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,6 +6,16 @@ public class HomeController : Controller
 {
     public IActionResult Index()
     {
+        ViewBag.paquetes =OrtWorld.Paquetes;
+        return View();
+    }
+    public IActionResult SelectPaquete(){
+
+        ViewBag.ListaAereos =OrtWorld.ListaAereos;
+        ViewBag.ListaDestinos =OrtWorld.ListaDestinos;
+        ViewBag.ListaHoteles =OrtWorld.ListaHotles;
+        ViewBag.ListaExcursiones =OrtWorld.ListaExcursiones;
+
         return View();
     }
 }
