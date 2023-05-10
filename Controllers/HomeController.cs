17e8ff7a593e445a6b609ac6138309ac4ce8fb22
DANTE VERDI GUTIERREZ <47573312@ort>
2023-05-10 08:58:12 -0300

    no se ven las imagenes y falta verificar que no exista paquete

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -23,14 +23,17 @@ public class HomeController : Controller
 
         if (destino > 10 || destino <1 || hotel>10 || hotel<1 || aereo>10 || aereo<1 || excursion>10 || excursion<1) {
             ViewBag.exeption = "Error: datos no validos";
-            return View(SelectPaquete);
+            return View("SelectPaquete");
         }
+        else {
         Paquete p = new Paquete(OrtWorld.ListaHotles[hotel-1], OrtWorld.ListaAereos[aereo-1], OrtWorld.ListaExcursiones[excursion-1]);
         bool e = OrtWorld.IngresarPaquete(OrtWorld.ListaDestinos[destino-1], p);
         ViewBag.ListaAereos =OrtWorld.ListaAereos;
         ViewBag.ListaDestinos =OrtWorld.ListaDestinos;
         ViewBag.ListaHoteles =OrtWorld.ListaHotles;
         ViewBag.ListaExcursiones =OrtWorld.ListaExcursiones;
-        return View(Index);
+        ViewBag.h=true;
+        ViewBag.paquetes=OrtWorld.Paquetes;
+        return View("Index");}    
     }
 }
--- Models/OrtWorld.cs
+++ Models/OrtWorld.cs
@@ -2,9 +2,9 @@ class OrtWorld {
 
 
     static public List<string> ListaDestinos {get; private set;} = new List<string> {"Ushuaia", "Lima", "Ciudad del Cabo", "Berlin", "San Petersburgo", "Kabul", "San Fransisco", "Mar del Plata", "Avellaneda", "Isla Rapa Nui"};
-    static public List<string> ListaHotles {get; private set;} = new List<string> {"hotel00.jfif","hotel01.jfif","hotel02.jfif","hotel03.jfif","hotel04.jfif","hotel06.jfif","hotel06.jfif","hotel05.jfif","hotel08.jfif","hotel09.jfif"};
-    static public List<string> ListaAereos {get; private set;} =new List<string> {"aereos00.jfif", "aereos01.jfif", "aereos02.jfif", "aereos03.jpg", "aereos04.jfif", "aereos05.jfif", "aereos06.jfif", "aereos07.jfif", "aereos08.jfif", "aereos09.jfif"};
-    static public List<string> ListaExcursiones {get; private set;} = new List<string>  {"excur00.jfif", "excur01.jfif", "excur02.jfif", "excur03.jfif", "excur04.jfif", "excur05.jpg", "excur06.jfif", "excur07.jfif", "excur08.jfif", "excur09.jfif"};
+    static public List<string> ListaHotles {get; private set;} = new List<string> {"~/img/hotel00.jfif","~/img/hotel01.jfif","~/img/hotel02.jfif","~/img/hotel03.jfif","~/img/hotel04.jfif","~/img/hotel06.jfif","~/img/hotel06.jfif","~/img/hotel05.jfif","~/img/hotel08.jfif","~/img/hotel09.jfif"};
+    static public List<string> ListaAereos {get; private set;} =new List<string> {"~/img/aereos00.jfif", "~/img/aereos01.jfif", "~/img/aereos02.jfif", "~/img/aereos03.jpg", "~/img/aereos04.jfif", "~/img/aereos05.jfif", "~/img/aereos06.jfif", "~/img/aereos07.jfif", "~/img/aereos08.jfif", "~/img/aereos09.jfif"};
+    static public List<string> ListaExcursiones {get; private set;} = new List<string>  {"~/img/excur00.jfif", "~/img/excur01.jfif", "~/img/excur02.jfif", "~/img/excur03.jfif", "~/img/excur04.jfif", "~/img/excur05.jpg", "~/img/excur06.jfif", "~/img/excur07.jfif", "~/img/excur08.jfif", "~/img/excur09.jfif"};
     static public Dictionary<string,Paquete> Paquetes {get; private set;} = new Dictionary<string, Paquete>();
 
     
