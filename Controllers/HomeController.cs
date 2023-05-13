6f0a984da0ca9d79ef129583bf602e6b7e15106f
unknown <danosqui20@gmail.com>
2023-05-12 21:30:13 -0300

    cosas

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -32,7 +32,7 @@ public class HomeController : Controller
         }
         else {
         Paquete p = new Paquete(OrtWorld.ListaHotles[hotel-1], OrtWorld.ListaAereos[aereo-1], OrtWorld.ListaExcursiones[excursion-1]);
-        bool e = OrtWorld.IngresarPaquete(OrtWorld.ListaDestinos[destino-1], p);
+        OrtWorld.IngresarPaquete(OrtWorld.ListaDestinos[destino-1], p);
 
         ViewBag.h=true;
         ViewBag.paquetes=OrtWorld.Paquetes;
