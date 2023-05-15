e34091e776acb46280afa82706ac104528351d5f
DANTE VERDI GUTIERREZ <47573312@ort>
2023-05-15 08:27:07 -0300

    optimizacion si se le puede llamar asi

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -7,7 +7,7 @@ public class HomeController : Controller
     public IActionResult Index()
     {
         ViewBag.paquetes =OrtWorld.Paquetes;
-        ViewBag.h=OrtWorld.Paquetes.Count() == 0 ? true : false;
+        ViewBag.h=OrtWorld.hayPaquetes;
         return View();
     }
     public IActionResult SelectPaquete(){
@@ -22,19 +22,20 @@ public class HomeController : Controller
     }
     public IActionResult GuardarPaquete(int destino, int hotel, int aereo, int excursion) {
 
-        ViewBag.ListaAereos =OrtWorld.ListaAereos;
-        ViewBag.ListaDestinos =OrtWorld.ListaDestinos;
-        ViewBag.ListaHoteles =OrtWorld.ListaHotles;
-        ViewBag.ListaExcursiones =OrtWorld.ListaExcursiones;
+        
         ViewBag.error= destino > 10 || destino <1 || hotel>10 || hotel<1 || aereo>10 || aereo<1 || excursion>10 || excursion<1;
         if (ViewBag.error) {
+            ViewBag.ListaAereos =OrtWorld.ListaAereos;
+            ViewBag.ListaDestinos =OrtWorld.ListaDestinos;
+            ViewBag.ListaHoteles =OrtWorld.ListaHotles;
+            ViewBag.ListaExcursiones =OrtWorld.ListaExcursiones;
             return View("Selectpaquete");
         }
         else {
         Paquete p = new Paquete(OrtWorld.ListaHotles[hotel-1], OrtWorld.ListaAereos[aereo-1], OrtWorld.ListaExcursiones[excursion-1]);
         OrtWorld.IngresarPaquete(OrtWorld.ListaDestinos[destino-1], p);
 
-        ViewBag.h=true;
+        ViewBag.h=OrtWorld.hayPaquetes;
         ViewBag.paquetes=OrtWorld.Paquetes;
         return View("Index");}    
     }
--- Models/OrtWorld.cs
+++ Models/OrtWorld.cs
@@ -6,14 +6,14 @@ class OrtWorld {
     static public List<string> ListaAereos {get; private set;} =new List<string> {"~/img/aereos00.jfif", "~/img/aereos01.jfif", "~/img/aereos02.jfif", "~/img/aereos03.jpg", "~/img/aereos04.jfif", "~/img/aereos05.jfif", "~/img/aereos06.jfif", "~/img/aereos07.jfif", "~/img/aereos08.jfif", "~/img/aereos09.jfif"};
     static public List<string> ListaExcursiones {get; private set;} = new List<string>  {"~/img/excur00.jfif", "~/img/excur01.jfif", "~/img/excur02.jfif", "~/img/excur03.jfif", "~/img/excur04.jfif", "~/img/excur05.jpg", "~/img/excur06.jfif", "~/img/excur07.jfif", "~/img/excur08.jfif", "~/img/excur09.jfif"};
     static public Dictionary<string,Paquete> Paquetes {get; private set;} = new Dictionary<string, Paquete>();
-
+    static public bool hayPaquetes {get; private set;}= false;
     
     public static bool IngresarPaquete(string destinoSeleccionado, Paquete paquete) {
         bool e;
 
         e=Paquetes.ContainsKey(destinoSeleccionado);
         if (!e) Paquetes.Add(destinoSeleccionado, paquete);
-
+        hayPaquetes=true;
         return e;
     }
 }
